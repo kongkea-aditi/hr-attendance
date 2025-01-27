ad5904dea00ebac915c50c9e24dda854e3df5870
kk <kongkea.ouch@camsolution.biz>
2025-01-27 13:13:57 +0700

    [IMP] hr_attendance_work_location_ip_check: Hide ip check options on work location when global setting disabled

--- hr_attendance_work_location_ip_check/models/hr_work_location.py
+++ hr_attendance_work_location_ip_check/models/hr_work_location.py
@@ -1,6 +1,10 @@
+import logging
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools.safe_eval import const_eval
+
+_logger = logging.getLogger(__name__)
 
 
 class HrWorkLocation(models.Model):
@@ -17,6 +21,19 @@ class HrWorkLocation(models.Model):
         default=False,
         groups="hr.group_hr_manager",
     )
+
+    global_check_ip = fields.Boolean(
+        string="IP Check Enabled Globally",
+        compute="_compute_ip_check_enabled",
+    )
+
+    def _compute_ip_check_enabled(self):
+        for record in self:
+            record.global_check_ip = (
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param("hr_attendance.ip_check_enabled")
+            )
 
     @api.constrains("check_ip", "allowed_cidr_ids")
     def _check_ip_cidrs(self):
